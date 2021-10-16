00e76836542318dbe6ea59a6dbb815080d9fe8c7
Carsio Eddyo <carsio.eddyo@fpf.br>
2021-10-16 01:43:53 -0400

    Change Ola function

--- ola_test.go
+++ ola_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola()
-	esperado := "Olá, mundo"
+	resultado := Ola("Carsio")
+	esperado := "Olá, Carsio"
 
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
